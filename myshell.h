1e919c59521395636942f93f9762e15aa3f147ef
Peter Zheng <scholarpetez@gmail.com>
2020-03-03 22:42:49 -0500

    Added utility.c that holds the external command functions. Added header file called myshell.h to hold function prototypes of utility.c

---
+++ myshell.h
@@ -0,0 +1,6 @@
+#ifndef MY_SHELL_H
+#define MY_SHELL_H
+
+void command_cd(char*);
+
+#endif
---
+++ utility.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "myshell.h"
+
+void command_cd(char* directory_name){
+
+	
+
+}
+
